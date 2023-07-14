04028aed518a2da13c46f82f2bb1b92df25b1a82
theactualcharliebrown <rooterboy.cb@gmail.com>
2023-07-14 12:40:01 -0500

    Fade-slide-addition

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,3 +2,23 @@ function toggler() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+
+ // Write your code here
+const items = document.querySelectorAll('.grid-item')
+
+const options = {
+  threshold: 0.5
+}
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    }
+  });
+}, options)
+
+items.forEach(item => {
+  observer.observe(item);
+})
