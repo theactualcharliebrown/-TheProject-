7a9b227277e4a9d7b3d8fab595cb128639224681
theactualcharliebrown <rooterboy.cb@gmail.com>
2023-08-22 21:05:27 -0500

    google-api-project

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -132,6 +132,116 @@ sortButton.addEventListener("click", () => {
   const sortedList = mergeSort(inputValues);
 
   output.innerHTML = `<p>Sorted List: ${sortedList.join(", ")}</p>`;
+  inputList.value = "";
 });
 
+// Playing with Google Maps API
+var directionsService;
+    var directionsRenderer
+    var map;
+    var geocoder;
+    var infoWindow;
 
+  // Map initialization
+  function initMap() {
+      directionsService = new google.maps.DirectionsService();
+      directionsRenderer = new google.maps.DirectionsRenderer();
+      geocoder = new google.maps.Geocoder();
+
+      const coordinates = { lat: 35.221050, lng: -97.445938};
+
+      map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 13,
+          center: coordinates,
+      });
+      directionsRenderer.setMap(map);
+
+      map.addListener("click", (e) => {
+        alert("You clicked the map at " + JSON.stringify(e.latLng.toJSON(), null, 2));
+        console.log(e.latLng);
+        document.getElementById('circleLat').value = e.latLng.lat();
+        document.getElementById('circleLong').value = e.latLng.lng();
+      });
+      }
+
+  //Place Marker
+  function placeMarker() {
+      const address = document.getElementById('markerAddress').value;
+
+    geocoder.geocode({ 'address': address }, (results, status) => {
+      if (status === 'OK') {
+        const location = results[0].geometry.location;
+        new google.maps.Marker({
+          map: map,
+          position: location,
+        });
+        map.setCenter(location);
+      } else {
+        alert('Error: ' + status);
+      }
+    });
+  }
+
+  // Route Calculator
+  function calcRoute() {
+      var start = document.getElementById('origin').value;
+      var end = document.getElementById('destination').value;
+      var request = {
+          origin: start,
+          destination: end,
+          travelMode: 'DRIVING'
+      };
+      directionsService.route(request, function (result, status) {
+          if (status == 'OK') {
+              directionsRenderer.setDirections(result);
+          } else { alert("An unexpected error occurred")}
+      });
+   }
+    
+  // Create Circle
+      function createCircle() {
+        const latitude = parseFloat(document.getElementById('circleLat').value);
+        const longitude = parseFloat(document.getElementById('circleLong').value);
+        const radius = parseFloat(document.getElementById('circleRad').value);
+        const circleOptions = {
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: { lat: latitude, lng: longitude },
+            radius: radius
+        };
+        new google.maps.Circle(circleOptions);
+        
+        map.setCenter({ lat: latitude, lng: longitude }); 
+    }
+
+
+  // Create Rectangle
+      function createRectangle() {
+        const North = parseFloat(document.getElementById('rectLat1').value);
+        const South = parseFloat(document.getElementById('rectLat2').value);
+        const East = parseFloat(document.getElementById('rectLong1').value);
+        const West = parseFloat(document.getElementById('rectLong2').value);
+
+        const rectangleOptions = {
+            strokeColor: '#0000FF',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#0000FF',
+            fillOpacity: 0.35,
+            map: map,
+            bounds: {
+                north: North,
+                south: South,
+                east: East,
+                west: West
+            }
+        };
+        new google.maps.Rectangle(rectangleOptions);
+      }
+
+
+  
